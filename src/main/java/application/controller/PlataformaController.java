5474e36a31a22b03efef0f09fde0f10bbb5da20d
AmabilieMabi <vitoriaamabiliebotazolimacedom@gmail.com>
2024-11-07 17:45:30 +0000

    vviviv

--- src/main/java/application/controller/PlataformaController.java
+++ src/main/java/application/controller/PlataformaController.java
@@ -1,8 +1,11 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,7 @@ import application.model.Plataforma;
 import application.record.PlataformaDTO;
 import application.repository.PlataformaRepository;
 import application.service.PlataformaService;
+import ch.qos.logback.core.model.processor.PhaseIndicator;
 
 @RestController
 @RequestMapping("/plataformas")
@@ -26,5 +30,15 @@ public class PlataformaController {
     @PostMapping
     public PlataformaDTO insert(@RequestBody PlataformaDTO plataforma) {
         return plataformaSrv.insertOne(plataforma);
+    }
+
+    @DeleteMapping("{/id}")
+    public void delete(@PathVariable long id) {
+        plataformaSrv.removeByid(id);
+    }
+
+    @PutMapping("{/id}")
+    public PlataformaDTO update(@PathVariable long id, @RequestBody PlataformaDTO plataforma){
+        return plataformaSrv.update(id, plataforma);
     }
 }
---
+++ src/main/java/application/model/Jogo.java
@@ -0,0 +1,37 @@
+package application.model;
+
+import java.util.HashSet;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Jogo {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+        @Column(nullable = false)
+        private String titulo;
+
+        @ManyToMany
+        @JoinTable(name = "jogos_possuem_plataformas")
+            JoinColumns = @JoinColumn(name = "id_jogo"),
+            inverseJoinColumns = @JoinColumn(name = "id_plataforma")
+   
+
+        private Set<Plataforma> plataforma = new HashSet<Plataforma>();
+        
+    }
+
